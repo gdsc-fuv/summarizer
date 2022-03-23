4acc4e5941b336503910dabab8164d18b89f66fb
LanPhuong <phuong.pham.210120@student.fulbright.edu.vn>
2022-03-23 20:29:02 +0700

    created some widgets & helper function to style ui

---
+++ lib/constants/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primBlue = Color(0xff1E2454);
+  static const primDarkGrey = Color(0xff202020);
+  static const primLightGrey = Color(0xff8A8A8A);
+  static const secDarkBlue = Color(0xff2B89D6);
+  static const secLightBlue = Color(0xff8FB8CC);
+  static const secGreen = Color(0xffCADF88);
+  static const penYellow = Color(0xffFFF3B7);
+  static const penBlue = Color(0xff8FB8CC);
+  static const penRed = Color(0xffDF8D88);
+  static const penGreen = Color(0xffCADF88);
+  static const penOrange = Color(0xffFFCE70);
+  static const statusGreen = Color(0xff69C561);
+  static const statusRed = Color(0xffDA5252);
+  static const statusYellow = Color(0xffF8C134);
+}
---
+++ lib/constants/images.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Image addFolderLogo = Image.asset("assets/add_folder.png");
+Image cancelPresentationLogo = Image.asset("assets/cancel_presentation.png");
+Image createSubfolderLogo = Image.asset("assets/create_subfolder.png");
+Image trashbinLogo = Image.asset("assets/delete.png");
+Image humanFolderLogo = Image.asset("assets/human_folder.png");
+Image ideaLogo = Image.asset("assets/idea.png");
+Image importFolderLogo = Image.asset("assets/import_folder.png");
+Image logoutLogo = Image.asset("assets/logout_white.png");
+Image moveToFolderLogo = Image.asset("assets/move_to_folder.png");
+Image pdfLogo = Image.asset("assets/pdf_logo.png");
+Image renameLogo = Image.asset("assets/rename_logo.png");
+Image rotateFileLogo = Image.asset("assets/rotate.png");
+Image uploadFileLogo = Image.asset("assets/upload_file.png");
+Image welcomeImage = Image.asset("assets/welcome_image.png");
