391a0a637612dae34831edc9d04297e1391fba6a
Giang2003 <89984724+Giang2003@users.noreply.github.com>
2022-03-29 18:13:04 +0700

    wip

--- lib/widgets/left_panel.dart
+++ lib/widgets/left_panel.dart
@@ -1,4 +1,74 @@
 // Giang & Phuong se code o day nhe
+// import 'package:flutter/material.dart';
+
+// class NavDrawer extends StatefulWidget {
+//   const NavDrawer({Key? key}) : super(key: key);
+
+//   @override
+//   State<NavDrawer> createState() => _NavDrawerState();
+// }
+
+// class _NavDrawerState extends State<NavDrawer> {
+//   final ScrollController _firstController = ScrollController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     const double searchContainerHeight = 110;
+
+//     return LayoutBuilder(
+//         builder: (BuildContext context, BoxConstraints constraints) {
+//       return Drawer(
+//         child: ListView(
+//           children: [
+//             const SizedBox(
+//               height: searchContainerHeight,
+//               child: DrawerHeader(
+//                 padding: EdgeInsets.symmetric(horizontal: 20),
+//                 margin: EdgeInsets.symmetric(vertical: 20),
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     border: OutlineInputBorder(),
+//                     prefixIcon: Icon(Icons.add_circle_outline),
+//                     hintText: 'Enter folder title   ',
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               height:
+//                   MediaQuery.of(context).size.height - searchContainerHeight,
+//               padding: const EdgeInsets.only(bottom: 10),
+//               child: ListView.builder(
+//                   // physics: ClampingScrollPhysics(),
+//                   controller: _firstController,
+//                   itemCount: 20,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return ExpansionTile(
+//                       initiallyExpanded: false,
+//                       leading: const Icon(Icons.folder),
+//                       title: Text('Folder $index'),
+//                       trailing: IconButton(
+//                           icon: const Icon(Icons.more_horiz), onPressed: () {}),
+//                       children: [
+//                         ListTile(
+//                             onTap: () {},
+//                             leading: const Icon(Icons.folder),
+//                             title: Text('Folder $index'),
+//                             contentPadding: const EdgeInsets.only(left: 50),
+//                             trailing: IconButton(
+//                                 icon: const Icon(Icons.more_horiz),
+//                                 onPressed: () {})),
+//                       ],
+//                     );
+//                   }),
+//             ),
+//           ],
+//         ),
+//       );
+//     });
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class NavDrawer extends StatefulWidget {
@@ -47,8 +117,19 @@ class _NavDrawerState extends State<NavDrawer> {
                       initiallyExpanded: false,
                       leading: const Icon(Icons.folder),
                       title: Text('Folder $index'),
-                      trailing: IconButton(
-                          icon: const Icon(Icons.more_horiz), onPressed: () {}),
+                      trailing: PopupMenuButton(
+                        icon: const Icon(Icons.more_horiz),
+                        itemBuilder: (context) => [
+                          const PopupMenuItem(
+                            child: Text("First"),
+                            value: 1,
+                          ),
+                          const PopupMenuItem(
+                            child: Text("Second"),
+                            value: 2,
+                          )
+                        ],
+                      ),
                       children: [
                         ListTile(
                             onTap: () {},
@@ -68,66 +149,3 @@ class _NavDrawerState extends State<NavDrawer> {
     });
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// class NavDrawer extends StatefulWidget {
-//   const NavDrawer({Key? key}) : super(key: key);
-
-//   @override
-//   State<NavDrawer> createState() => _NavDrawerState();
-// }
-
-// class _NavDrawerState extends State<NavDrawer> {
-//   final ScrollController _firstController = ScrollController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     const double searchContainerHeight = 110;
-
-//     return LayoutBuilder(
-//         builder: (BuildContext context, BoxConstraints constraints) {
-//       return Drawer(
-//         child: ListView(
-//           children: [
-//             const SizedBox(
-//               height: searchContainerHeight,
-//               child: DrawerHeader(
-//                 padding: EdgeInsets.symmetric(horizontal: 20),
-//                 margin: EdgeInsets.symmetric(vertical: 20),
-//                 child: TextField(
-//                   decoration: InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     prefixIcon: Icon(Icons.add_circle_outline),
-//                     hintText: 'Enter folder title   ',
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             Container(
-//               height:
-//                   MediaQuery.of(context).size.height - searchContainerHeight,
-//               padding: const EdgeInsets.only(bottom: 10),
-//               child: ListView.builder(
-//                   // physics: ClampingScrollPhysics(),
-//                   controller: _firstController,
-//                   itemCount: 20,
-//                   itemBuilder: (BuildContext context, int index) {
-//                     return DropdownButton(
-//                       child: ListTile(
-//                           onTap: () {},
-//                           leading: const Icon(Icons.folder),
-//                           title: Text('Folder $index'),
-//                           trailing: IconButton(
-//                               icon: const Icon(Icons.more_horiz),                            
-//                               onPressed: () {})
-//                       ),
-//                     );                            
-//                   }),
-//             ),
-//           ],
-//         ),
-//       );
-//     });
-//   }
-// }
